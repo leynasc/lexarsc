64ab94a22a8fd24ac0c0bf827af5abe57fcc58c9
leynasc <114754126+leynasc@users.noreply.github.com>
2022-09-30 21:10:14 +0700

    Create echo.php

---
+++ echo/echo.php
@@ -0,0 +1,9 @@
+ echo 1 <=> 1; //output  0
+    echo 3 <=> 4; //output -1
+    echo 4 <=> 3; //output  1
+
+    //String Comparison
+
+    echo "x" <=> "x"; //output  0
+    echo "x" <=> "y"; //output -1
+    echo "y" <=> "x"; //output  1
